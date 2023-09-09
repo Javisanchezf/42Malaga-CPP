4b6b9f67f386c45e62ae1d863f8cbc558e5d8544
javiersa <javiersa@student.42malaga.com>
2023-09-09 21:14:50 +0200

    [2023-09-09 21:14:50] - Little changes by javiersa

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: javiersa <javiersa@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 19:57:50 by javiersa          #+#    #+#             */
-/*   Updated: 2023/09/09 20:57:19 by javiersa         ###   ########.fr       */
+/*   Updated: 2023/09/09 21:14:25 by javiersa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,16 @@ std::string	cin_plus(std::string str)
 int	main(void)
 {
 	std::cout << BLUE "Creating zombie on the stack.\n" DEFAULT;
+	Zombie zombi0(cin_plus("Zombie name: "));
+	std::cout << BLUE "Creating other zombie on the stack.\n" DEFAULT;
 	Zombie zombi1(cin_plus("Zombie name: "));
+	std::cout << BLUE "Calling announce().\n" DEFAULT;
+	zombi1.announce();
 
 	std::cout << BLUE "\nCalling newZombie().\n" DEFAULT;
 	Zombie *zombi2 = newZombie(cin_plus("Zombie name: "));
-	std::cout << BLUE "Calling announce().\n" DEFAULT;
-	zombi2->announce();
+	std::cout << BLUE "Freeing allocated memory on heap.\n" DEFAULT;
+	delete zombi2;
 
 	std::cout << BLUE "\nCalling randomChump().\n" DEFAULT;
 	randomChump(cin_plus("Zombie name: "));
