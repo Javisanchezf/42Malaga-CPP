fb7ddac6b22106bbddd835179f775805b39e20c0
javisanchezf <javiersa@student.42malaga.com>
2023-09-10 14:03:19 +0200

    Misc and ex02

--- CPP01/ex00/Zombie.cpp
+++ CPP01/ex00/Zombie.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Zombie.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: javiersa <javiersa@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: javiersa <javiersa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 18:33:40 by javiersa          #+#    #+#             */
-/*   Updated: 2023/09/09 20:31:59 by javiersa         ###   ########.fr       */
+/*   Updated: 2023/09/10 13:28:03 by javiersa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ Zombie::~Zombie()
 {
 	std::cout << "Zombie " << this->_name << " has been destroyed.\n";
 }
-void Zombie::announce(void)
+void	Zombie::announce(void)
 {
 	std::cout << this->_name <<": BraiiiiiiinnnzzzZ...\n";
 }
