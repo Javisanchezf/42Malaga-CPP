240ea8998860d36565c5c8c6680d76acbe151c34
javiersa <javiersa@student.42malaga.com>
2023-12-11 20:21:42 +0100

    [2023-12-11 20:21:42] - Little changes by javiersa

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: javiersa <javiersa@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 11:14:41 by javiersa          #+#    #+#             */
-/*   Updated: 2023/12/11 20:18:30 by javiersa         ###   ########.fr       */
+/*   Updated: 2023/12/11 20:21:04 by javiersa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,9 @@ int main(void)
 	rrf2 = someRandomIntern.makeForm("shrubbery creation", "Bender2");
 	rrf3 = someRandomIntern.makeForm("shrubbery creation", "Bender3");
 	rrf4 = someRandomIntern.makeForm("sdfsdg", "Bender4");
+	Bureaucrat b("Bender", 1);
+	b.signForm(*rrf2);
+	rrf2->execute(b);
 	delete rrf;
 	delete rrf2;
 	delete rrf3;
