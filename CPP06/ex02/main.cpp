5faef358ef2f7ebf9d719b753f4c5ebd197e8268
javiersa <javiersa@student.42malaga.com>
2023-12-19 19:08:11 +0100

    [2023-12-19 19:08:11] - Little changes by javiersa

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -1,5 +1,64 @@
-#include "RealType.hpp"
+#include <iostream>
+#include <string>
+#include <ctime>
 
+class Base {public: virtual ~Base() {}};
+class A: public Base {};
+class B: public Base {};
+class C: public Base {};
+
+Base *generator(void)
+{
+	int i = rand() % 3;
+	if (i == 0)
+		return new A;
+	else if (i == 1)
+		return new B;
+	else
+		return new C;
+}
+
+void identify(Base *p)
+{
+	try
+	{
+		// std::cout << "Identify by Pointer: ";
+		if (dynamic_cast<A*>(p))
+			std::cout << "A" << std::endl;
+		else if (dynamic_cast<B*>(p))
+			std::cout << "B" << std::endl;
+		else if (dynamic_cast<C*>(p))
+			std::cout << "C" << std::endl;
+		else
+			std::cout << "Unknown" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+}
+
+void identify(Base &p)
+{
+    
+    try
+	{
+    	// std::cout << "Identify by Reference: ";
+		if (A *a = dynamic_cast<A*>(&p))
+			std::cout << "A" << std::endl;
+		else if (B *b = dynamic_cast<B*>(&p))
+			std::cout << "B" << std::endl;
+		else if (C *c = dynamic_cast<C*>(&p))
+			std::cout << "C" << std::endl;
+		else
+			std::cout << "Unknown" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
 int main(void)
 {
 	srand(time(NULL));
