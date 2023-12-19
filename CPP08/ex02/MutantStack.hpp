57ab765ee7b4fef632d916e88d6b8fe6f6542078
javiersa <javiersa@student.42malaga.com>
2023-12-19 21:30:13 +0100

    [2023-12-19 21:30:13] - Little changes by javiersa

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <deque>
+
+template <typename T>
+class MutantStack: public std::stack<T>
+{
+	public:
+		MutantStack(): std::stack<T>() {}
+		MutantStack(const MutantStack &copy): std::stack<T>(copy) {}
+		~MutantStack() {}
+		MutantStack &operator=(const MutantStack &copy)
+		{
+			if (this != &copy)
+				std::stack<T>::operator=(copy);
+			return *this;
+		}
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin()
+		{
+			return std::stack<T>::c.begin();
+		}
+		iterator end()
+		{
+			return std::stack<T>::c.end();
+		}
+};
+
+#endif
