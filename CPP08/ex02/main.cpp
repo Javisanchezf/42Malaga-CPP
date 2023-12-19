ee594b827431407cc9e62495259655716d2230a6
javiersa <javiersa@student.42malaga.com>
2023-12-19 21:47:25 +0100

    [2023-12-19 21:47:25] - Little changes by javiersa

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -20,28 +20,28 @@ int main()
 		std::cout << *it << std::endl;
 		++it;
 	}
-	// std::cout << "-----------------------------" << std::endl;
-	// std::cout << "Copy constructor:" << std::endl;
-	// MutantStack<int> mstack1(mstack);
-	// std::cout << "Size: " << mstack1.size() << std::endl;
-	// MutantStack<int>::iterator it1 = mstack1.begin();
-	// MutantStack<int>::iterator ite1 = mstack1.end();
-	// std::cout << "\nIterator: " << std::endl;
-	// while (it1 != ite1)
-	// {
-	// 	std::cout << *it1 << std::endl;
-	// 	++it1;
-	// }
-	// std::cout << "-----------------------------" << std::endl;
-	// std::cout << "Assignation operator:" << std::endl;
-	// MutantStack<int> mstack2 = mstack;
-	// std::cout << "Size: " << mstack2.size() << std::endl;
-	// MutantStack<int>::iterator it2 = mstack2.begin();
-	// MutantStack<int>::iterator ite2 = mstack2.end();
-	// std::cout << "\nIterator: " << std::endl;
-	// while (it2 != ite2)
-	// {
-	// 	std::cout << *it2 << std::endl;
-	// 	++it2;
-	// }
+	std::cout << "-----------------------------" << std::endl;
+	std::cout << "Copy constructor:" << std::endl;
+	MutantStack<int> mstack1(mstack);
+	std::cout << "Size: " << mstack1.size() << std::endl;
+	MutantStack<int>::iterator it1 = mstack1.begin();
+	MutantStack<int>::iterator ite1 = mstack1.end();
+	std::cout << "\nIterator: " << std::endl;
+	while (it1 != ite1)
+	{
+		std::cout << *it1 << std::endl;
+		++it1;
+	}
+	std::cout << "-----------------------------" << std::endl;
+	std::cout << "Assignation operator:" << std::endl;
+	MutantStack<int> mstack2 = mstack;
+	std::cout << "Size: " << mstack2.size() << std::endl;
+	MutantStack<int>::iterator it2 = mstack2.begin();
+	MutantStack<int>::iterator ite2 = mstack2.end();
+	std::cout << "\nIterator: " << std::endl;
+	while (it2 != ite2)
+	{
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
 }
