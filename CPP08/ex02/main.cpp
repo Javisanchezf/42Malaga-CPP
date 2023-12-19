3dc4764487c2ce9a62c2828a789f3e75ab62171e
javiersa <javiersa@student.42malaga.com>
2023-12-19 21:46:51 +0100

    [2023-12-19 21:46:51] - Little changes by javiersa

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -5,24 +5,43 @@ int main()
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	mstack.push(170);
-	std::cout << "Top: "<< mstack.top() << std::endl;
+	std::cout << "Top: " << mstack.top() << std::endl;
 	mstack.pop();
 	std::cout << "Size: " << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
+	std::cout << "\nIterator: " << std::endl;
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
 		++it;
 	}
-	std::stack<int> s(mstack);
-	return 0;
+	// std::cout << "-----------------------------" << std::endl;
+	// std::cout << "Copy constructor:" << std::endl;
+	// MutantStack<int> mstack1(mstack);
+	// std::cout << "Size: " << mstack1.size() << std::endl;
+	// MutantStack<int>::iterator it1 = mstack1.begin();
+	// MutantStack<int>::iterator ite1 = mstack1.end();
+	// std::cout << "\nIterator: " << std::endl;
+	// while (it1 != ite1)
+	// {
+	// 	std::cout << *it1 << std::endl;
+	// 	++it1;
+	// }
+	// std::cout << "-----------------------------" << std::endl;
+	// std::cout << "Assignation operator:" << std::endl;
+	// MutantStack<int> mstack2 = mstack;
+	// std::cout << "Size: " << mstack2.size() << std::endl;
+	// MutantStack<int>::iterator it2 = mstack2.begin();
+	// MutantStack<int>::iterator ite2 = mstack2.end();
+	// std::cout << "\nIterator: " << std::endl;
+	// while (it2 != ite2)
+	// {
+	// 	std::cout << *it2 << std::endl;
+	// 	++it2;
+	// }
 }
