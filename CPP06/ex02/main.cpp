87541eaa0a62f9c296653fba6c73c698683f7992
javiersa <javiersa@student.42malaga.com>
2023-12-19 18:53:48 +0100

    [2023-12-19 18:53:48] - Little changes by javiersa

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -4,9 +4,23 @@ int main(void)
 {
 	srand(time(NULL));
 	Base *p = generator();
-	// Base *p = nullptr;
+	Base *a = new A;
+	Base *b = new B;
+	Base *c = new C;
+	identify(a);
+	identify(*a);
+	identify(b);
+	identify(*b);
+	identify(c);
+	identify(*c);
+	std::cout << "---------------------------------" << std::endl;
+	std::cout << "Now the random: " << std::endl;
 	identify(p);
 	identify(*p);
+	delete a;
+	delete b;
+	delete c;
 	delete p;
+	// Base *p = nullptr;
 	return 0;
 }
