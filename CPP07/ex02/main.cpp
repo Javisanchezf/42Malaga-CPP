200f07cfd1b3904a9c3d60e5e4109c6fc47028fa
javiersa <javiersa@student.42malaga.com>
2023-12-21 19:15:29 +0100

    [2023-12-21 19:15:29] - Little changes by javiersa

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,46 +1,6 @@
 #include <iostream>
 #include "Array.hpp"
 
-// #define MAX_VAL 20
-// int main(int, char**)
-// {
-//     Array<int> numbers(MAX_VAL);
-//     int* mirror = new int[MAX_VAL];
-//     srand(time(NULL));
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         const int value = rand();
-//         numbers[i] = value;
-//         mirror[i] = value;
-//     }
-//     //SCOPE
-//     {
-//         Array<int> tmp = numbers;
-//         Array<int> test(tmp);
-//     }
-
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         if (mirror[i] != numbers[i])
-//         {
-//             std::cerr << RED "didn't save the same value!!" DEFAULT << std::endl;
-//             return 1;
-//         }
-// 		else
-// 			std::cout << GREEN "numbers[" << i << "] = " << numbers[i] << DEFAULT << std::endl;
-//     }
-//     delete [] mirror;//
-
-// 	Array<std::string> strings(4);
-// 	strings[0] = "Hello";
-// 	strings[1] = "World";
-// 	strings[2] = "!";
-// 	strings[3] = "How are you?";
-// 	for (unsigned int i = 0; i < strings.size(); i++)
-// 		std::cout << BLUE "strings[" << i << "] = " << strings[i] << DEFAULT << std::endl;	
-//     return 0;
-// }
-
 #define SIZE 20
 
 int main( void )
